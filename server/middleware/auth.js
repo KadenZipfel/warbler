cd24175074d7b4345520b7c12b282fc98bf97c55
unknown <kaden.zipfel@hotmail.com>
2018-11-19 13:40:15 -0800

    Add loginRequired middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,27 @@
+require('dotenv').load();
+const jwt = require('jsonwebtoken');
+
+exports.loginRequired = function(req, res, next) {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+      if(decoded) {
+        return next();
+      } else {
+        return next({
+          status: 401,
+          message: 'Please login first'
+        });
+      }
+    });
+  } catch(err) {
+    return next({
+      status: 401,
+      message: 'Please login first'
+    })
+  }
+};
+
+exports.ensureCorrectUser = function(req, res, next) {
+  
+};

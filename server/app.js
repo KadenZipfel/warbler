9b80efd3ad4e193d10e1827819446d402d75706c
unknown <kaden.zipfel@hotmail.com>
2018-11-19 12:10:42 -0800

    Store password securely

--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const errorHandler = require('./handlers/error');
+const authRoutes = require('./routes/auth');
 
 const PORT = 3000 || process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// Routes will go here later
+app.use('/api/auth', authRoutes);
 
 app.use((req, res, next) => {
   let err = new Error('Not Found');

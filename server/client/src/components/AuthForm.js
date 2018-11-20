a05adc0d81fdb0aa4bb6619203d9f472fe66aca5
unknown <kaden.zipfel@hotmail.com>
2018-11-20 12:16:34 -0800

    Add AuthForm component

---
+++ server/client/src/components/AuthForm.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+
+export default class AuthForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      username: '',
+      password: '',
+      profileImageUrl: ''
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  render() {
+    const {email, username, password, profileImageUrl} = this.state;
+    const {heading, buttonText, signUp} = this.props;
+    return (
+      <div>
+        <div className="row justify-content-md-center text-center">
+          <div className="col-md-6">
+            <form onSubmit={this.handleSubmit}>
+              <h2>{heading}</h2>
+              <label htmlFor="email">Email</label>
+              <input type="text" className="form-control"  value={email}
+              id="email" name="email" onChange={this.handleChange}/>
+              <label htmlFor="password">Password</label>
+              <input type="password" className="form-control"  value={password}
+              id="password" name="password" onChange={this.handleChange}/>
+              {signUp && (
+                <div>
+                  <label htmlFor="username">Username</label>
+                  <input type="text" className="form-control"  value={username}
+                  id="username" name="username" onChange={this.handleChange}/>
+                  <label htmlFor="image-url">Image URL:</label>
+                  <input type="text" className="form-control"  value={profileImageUrl}
+                  id="image-url" name="profileImageUrl" onChange={this.handleChange}/>
+                </div>
+              )}
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}

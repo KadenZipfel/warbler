e72e9b9200b8910ecd1bddc8328cbb3c633fc919
unknown <kaden.zipfel@hotmail.com>
2018-11-21 10:13:24 -0800

    Add JWT to HTTP headers

--- server/client/src/store/reducers/index.js
+++ server/client/src/store/reducers/index.js
@@ -1,6 +1,7 @@
 import {combineReducers} from 'redux';
 import currentUser from './currentUser';
 import errors from './errors';
+import messages from './messages';
 
 const rootReducer = combineReducers({
   currentUser,

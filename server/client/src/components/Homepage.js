33cd66afb31b9bbf835c33266c588d38be21b266
unknown <kaden.zipfel@hotmail.com>
2018-11-21 09:37:57 -0800

    Handle redirects

--- server/client/src/components/Homepage.js
+++ server/client/src/components/Homepage.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const Homepage = () => (
-  <div className="home-hero">
-    <h1>What's happening?</h1>
-    <h4>New to Warbler?</h4>
-    <Link to="/signup" className="btn btn-primary">Sign Up</Link>
-  </div>
-);
+const Homepage = ({currentUser}) => {
+  if(!currentUser.isAuthenticated) {
+    return (
+      <div className="home-hero">
+        <h1>What's happening?</h1>
+        <h4>New to Warbler?</h4>
+        <Link to="/signup" className="btn btn-primary">Sign Up</Link>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>You made it</h1>
+    </div>
+  );
+};
 
 export default Homepage;
